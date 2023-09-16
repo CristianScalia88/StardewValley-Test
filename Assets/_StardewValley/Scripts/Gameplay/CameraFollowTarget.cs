b2c5411dc04a66be757b29b3defa2e2c57e0eb4a
Cristian Scalia <cristiansk8x@gmail.com>
2023-09-16 12:00:15 -0300

    Camera starts the offset Applied

--- Assets/_StardewValley/Scripts/Gameplay/CameraFollowTarget.cs
+++ Assets/_StardewValley/Scripts/Gameplay/CameraFollowTarget.cs
@@ -6,6 +6,11 @@ public class CameraFollowTarget : MonoBehaviour
     [SerializeField] private float lerpTime = 10;
     [SerializeField] private Vector3 offset;
 
+    private void Start()
+    {
+        transform.position = target.position + offset;
+    }
+
     private void FixedUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.fixedDeltaTime * lerpTime);
